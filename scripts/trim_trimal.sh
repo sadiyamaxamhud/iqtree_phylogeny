6505def90b506f331905a5e0729152783fbd3145
sadiyamaxamhud <sadiyamaxamhud@hotmail.com>
2025-04-19 22:43:59 +0200

    Update trim_trimal.sh

--- scripts/trim_trimal.sh
+++ scripts/trim_trimal.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+# Description: Trims alignment using trimAl
+# Input: data/sequences_aligned.fasta
+# Output: data/sequences_trimmed.fasta
+
+echo " Trimming alignment with trimAl..."
+
+trimal -in ../data/sequences_aligned.fasta -out ../data/sequences_trimmed.fasta -gappyout -html ..../data/sequences_trimmed.html
+
+echo " Trimming complete: saved to data/sequences_trimmed.fasta"
 
